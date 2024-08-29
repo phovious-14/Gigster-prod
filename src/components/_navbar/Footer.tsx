a30cf77cee0417e458d2185e5090e8d377843614
phovious-14 <krishnpanchal14@gmail.com>
2024-08-29 22:03:25 +0530

    changed some ui and added loading effect

--- src/components/_navbar/Footer.tsx
+++ src/components/_navbar/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <section className="text-gray-700 bg-white body-font">
+    <section className="text-gray-700 bg-white body-font absolute bottom-0 w-full overflow-x-hidden">
       <div className="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
             <Link href="/" className="flex items-center mb-5 font-medium text-gray-900 lg:w-auto lg:items-center lg:justify-center md:mb-0">
               <span className="mx-auto text-xl font-black leading-none text-gray-900 select-none">
