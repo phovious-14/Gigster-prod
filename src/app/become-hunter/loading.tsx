e5b30bd3e44dbe4aa2ba2a87b03ad82834e1b446
phovious-14 <krishnpanchal14@gmail.com>
2024-10-26 23:45:20 +0530

    commit

--- src/app/become-hunter/loading.tsx
+++ src/app/become-hunter/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-    return <div className="h-screen w-screen flex justify-center items-center"><div className="loading"></div></div>
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="loading"></div>
+    </div>
+  );
 }
